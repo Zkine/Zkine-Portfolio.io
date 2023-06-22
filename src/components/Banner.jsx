2855c31ae692e5c9f3fa81e6d2c70141fd581b9d
Zkine <philippe_jaymes@hotmail.com>
2023-06-22 13:51:01 +0200

    Amélioration de l'accessibilité avec les attributs ARIA-* du bouton du composent Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,9 @@ import { useState } from "react";
 export default function Banner({ sidebarOpen, updateSidebar }) {
   const [clickBanner, setclickBanner] = useState(false);
 
-  function selected() {
+  function selected(element) {
+    let pressed = element.currentTarget.getAttribute("aria-pressed") === "true";
+    element.currentTarget.setAttribute("aria-pressed", !pressed);
     setclickBanner(!clickBanner);
     updateSidebar(!sidebarOpen);
   }
@@ -17,7 +19,8 @@ export default function Banner({ sidebarOpen, updateSidebar }) {
       <section className="banner">
         <h1 className="banner__titre">Philippe JAYMES - Portfolio</h1>
         <Button
-          aria-label="Button_menu"
+          aria-haspopup="menu"
+          aria-pressed="false"
           className={[clickBanner ? "active" : "banner--button"]}
           onClick={(e) => selected(e)}
         >
